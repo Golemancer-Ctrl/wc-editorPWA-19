6e6cc28e4eaa195b4cad62594dd1197b1d69d8ab
Sheev <ericmarcypro@gmail.com>
2023-01-17 23:38:49 -0500

    completed webpack config TODOs

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,7 +26,6 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
 registerRoute(
   // filter requests we want to cache (JS and CSS files)
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
